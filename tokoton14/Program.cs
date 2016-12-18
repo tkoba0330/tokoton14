3e042545c723d2e5de8e45a449e62723550e2a00
kobayashi tomoaki <t.koba0330@hotmail.co.jp>
2016-12-18 23:09:56 +0900

    学習中-とことんC#14 属性

---
+++ tokoton14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+     
+     
+
+namespace tokoton14
+{
+    class Program
+    {
+        [Conditional("DEBUG")]
+        private static void debugDump()
+        {
+            Console.WriteLine("Thia is a debug version.");
+        }
+
+        static void Main(string[] args)
+        {
+            debugDump();
+            Console.WriteLine("Hello");
+
+            Console.ReadLine();
+        }
+    }
+}
